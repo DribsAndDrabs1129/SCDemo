abe4001c047c5efa1c9b227b1c497f1b5c99dc80
Sunc <4919668@qq.com>
2017-12-01 11:22:26 +0800

    Initial Commit

---
+++ SCDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SCDemo
+//
+//  Created by Channe Sun on 2017/12/1.
+//  Copyright © 2017年 HUST. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SCDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SCDemo
+//
+//  Created by Channe Sun on 2017/12/1.
+//  Copyright © 2017年 HUST. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
